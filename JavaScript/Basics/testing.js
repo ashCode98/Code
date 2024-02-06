b5c36356fdb7fbfd9a781b121c430cbb27682503
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2024-02-06 19:35:03 +0530

    Added

--- JavaScript/Basics/testing.js
+++ JavaScript/Basics/testing.js
@@ -1,25 +1,50 @@
-let variableOne = Symbol("Ashish")
-let variableTwo = Symbol("Ashish")
+// let variableOne = Symbol("Ashish")
+// let variableTwo = Symbol("Ashish")
+//
+// let bigNumber = 8683579275902709726n
+//
+// const teamSoul = ["Omega", "Goblin", "Neyo", "Akshat"]
+//
+// let firstOne = 4;
+// let secondOne = 5;
+//
+// let userAshish = {
+//     Age: 18,
+//     city: "balasinor",
+//     email: "atankmember@gmail.com",
+//     upi: "ashish@ypi"
+// }
+//
+// let userTemporary = userAshish
+//
+// userTemporary.Age = 20
+// userTemporary.city = "Modasa"
+//
+// console.log(userAshish)
+//
+// console.log(userTemporary)
 
-let bigNumber = 8683579275902709726n
+// var createCounter = function(n) {
+//     console.log(`Outer function ${n}`);
+//     return function(){
+//         console.log(`Inner function ${n}`);
+//         return n++;
+//     };
+// };
+//
+// const counter = createCounter(10)
+// counter()
+// counter()
+// counter()
 
-const teamSoul = ["Omega", "Goblin", "Neyo", "Akshat"]
-
-let firstOne = 4;
-let secondOne = 5;
-
-let userAshish = {
-    Age: 18,
-    city: "balasinor",
-    email: "atankmember@gmail.com",
-    upi: "ashish@ypi"
+function createAdder(a) {
+    console.log(`a: ${a}`)
+    return function add(b) {
+        console.log(`b: ${b}`)
+        let sum;
+        sum = a + b;
+        return sum;
+    }
 }
-
-let userTemporary = userAshish
-
-userTemporary.Age = 20
-userTemporary.city = "Modasa"
-
-console.log(userAshish)
-
-console.log(userTemporary)
+const addTo2 = createAdder(2);
+addTo2(5); // 7
