662268480dc854ae34219bfddef1d886a3851fa4
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2024-06-05 08:01:22 +0530

    Added new things

--- CP and DSA/Competitive programming/Codeforces/main.cpp
+++ CP and DSA/Competitive programming/Codeforces/main.cpp
@@ -1,20 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
-    int n, x; cin >> n >> x;
-    
-    vector<int> v(n);
-    for(int i = 0; i<n; i++)cin >> v[i];
-    
-    if(n==1) cout << v[0] << endl;
-    else cout << x - n << endl;
-}
-
 int main() {
-	int t;
-	cin >> t;
-	while(t--){
-	    solve();
-	}
+    deque<int> q;
+    q.push_front(1);
+    q.push_front(2);
+    q.push_front(3);
+    q.push_front(4);
+
+    q.pop_front();
+
+    for(auto i: q){
+        cout << i << " ";
+    }
 }
---
+++ CP and DSA/Competitive programming/linked_list.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 2;
+    int* y = &n;
+    cout << *y;
+}
