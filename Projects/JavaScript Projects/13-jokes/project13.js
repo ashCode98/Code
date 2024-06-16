793126aaba85db5fbcf8c455b6339ab8e4c86246
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2024-06-16 19:32:25 +0530

    Added new files

---
+++ Projects/JavaScript Projects/13-jokes/project13.js
@@ -0,0 +1,26 @@
+const url = 'https://api.chucknorris.io/jokes/random';
+const Button = document.querySelector('.btn')
+const Dis = document.querySelector('#display-joke')
+const httpRequest = new XMLHttpRequest()
+
+Button.addEventListener('click', () => {
+    dis();
+})
+
+function dis() {
+    httpRequest.open('GET', url)
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4) {
+            const res = JSON.parse(this.responseText);
+            Dis.textContent = `"${res.value}"`
+        }
+    }
+
+    httpRequest.send()
+}
+
+
+// handle this end point with promises
+
+// handle the case of race condition
+
---
+++ Projects/JavaScript Projects/13-jokes/temp.js
@@ -0,0 +1,4 @@
+const url = 'https://api.chucknorris.io/jokes/random';
+
+const XHR = new XMLHttpRequest();
+console.log(XHR.response())
