bfe167b6485cefcba3de4884ed0d91c94e4484fe
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2024-06-18 17:12:21 +0530

    Added

--- Projects/JavaScript Projects/13-jokes/project13.js
+++ Projects/JavaScript Projects/13-jokes/project13.js
@@ -1,26 +1,40 @@
-const url = 'https://api.chucknorris.io/jokes/random';
-const Button = document.querySelector('.btn')
-const Dis = document.querySelector('#display-joke')
-const httpRequest = new XMLHttpRequest()
+// const url = 'https://api.chucknorris.io/jokes/random';
+// const Button = document.querySelector('.btn')
+// const Dis = document.querySelector('#display-joke')
 
-Button.addEventListener('click', () => {
-    dis();
-})
+// const httpRequest = new XMLHttpRequest()
 
-function dis() {
-    httpRequest.open('GET', url)
-    httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState == 4) {
-            const res = JSON.parse(this.responseText);
-            Dis.textContent = `"${res.value}"`
-        }
-    }
+// Button.addEventListener('click', () => {
+//     dis();
+// })
 
-    httpRequest.send()
-}
+// function dis() {
+//     httpRequest.open('GET', url)
+//     httpRequest.onreadystatechange = function () {
+//         if (httpRequest.readyState == 4) {
+//             const res = JSON.parse(this.responseText);
+//             Dis.textContent = `"${res.value}"`
+//         }
+//     }
+
+//     httpRequest.send()
+// }
 
 
 // handle this end point with promises
-
 // handle the case of race condition
 
+async function getJokes() {
+    try {
+        const response = await
+            fetch('https://api.chucknorris.io/jokes/random')
+
+        const data = await response.json()
+        console.log(data.value);
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+getJokes()
--- Projects/JavaScript Projects/13-jokes/temp.js
+++
@@ -1,4 +0,0 @@
-const url = 'https://api.chucknorris.io/jokes/random';
-
-const XHR = new XMLHttpRequest();
-console.log(XHR.response())
