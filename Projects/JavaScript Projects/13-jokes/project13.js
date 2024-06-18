b647899d2ae49b7eeb9d3c0e573ec336c4c8e912
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2024-06-18 20:45:52 +0530

    completed project13

--- Projects/JavaScript Projects/13-jokes/project13.js
+++ Projects/JavaScript Projects/13-jokes/project13.js
@@ -1,19 +1,22 @@
-// const url = 'https://api.chucknorris.io/jokes/random';
-// const Button = document.querySelector('.btn')
-// const Dis = document.querySelector('#display-joke')
+let url = 'https://official-joke-api.appspot.com/jokes/random';
+const Button = document.querySelector('.btn')
+const Dis = document.querySelector('#display-joke')
 
 // const httpRequest = new XMLHttpRequest()
 
-// Button.addEventListener('click', () => {
-//     dis();
-// })
+Button.addEventListener('click', () => {
+    // dis();
+    // getRandomJoke()
+})
 
 // function dis() {
 //     httpRequest.open('GET', url)
 //     httpRequest.onreadystatechange = function () {
 //         if (httpRequest.readyState == 4) {
 //             const res = JSON.parse(this.responseText);
-//             Dis.textContent = `"${res.value}"`
+//             console.log(res)
+//             Dis.innerHTML = `"${res.setup}" <br> ->
+//             <space-between> ${res.punchline}`
 //         }
 //     }
 
@@ -24,17 +27,9 @@
 // handle this end point with promises
 // handle the case of race condition
 
-async function getJokes() {
-    try {
-        const response = await
-            fetch('https://api.chucknorris.io/jokes/random')
+async function getRandomJoke() {
+    const response = await fetch(url)
+    const data = await response.json();
 
-        const data = await response.json()
-        console.log(data.value);
-    }
-    catch (e) {
-        console.log(e);
-    }
+    Dis.innerHTML = `"${data.setup}" <br> -> ${data.punchline}`
 }
-
-getJokes()
