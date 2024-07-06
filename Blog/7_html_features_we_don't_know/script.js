1aed38b548c0b4e30deeffa8f2c7dbaac69da1e3
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2024-07-06 16:16:51 +0530

    Added new files

---
+++ Blog/7_html_features_we_don't_know/script.js
@@ -0,0 +1,13 @@
+// const Button = document.querySelector('button')
+// const dialog = document.querySelector('dialog')
+
+// Button.addEventListener("click", () => {
+//     dialog.showModal();
+// })
+
+// const IN = document.querySelector('input')
+// const Btn = document.querySelector('#color')
+
+// Btn.addEventListener('click', () => {
+//     IN.showPicker();
+// })
--- CP and DSA/Competitive programming/TLE Eleminators/C++ STL/three.cpp
+++ CP and DSA/Competitive programming/TLE Eleminators/C++ STL/three.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// int fib(int n){
+// 	if(n == 1){
+// 		return 1;
+// 	}
+
+// 	int result = fib(n)*fib(n-1);
+// 	return result;
+// }
+
 int main(){
-    cout << "Something good";
+    
+    cout << "something" << endl;
+
 }
