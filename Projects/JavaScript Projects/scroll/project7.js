3e529c0fffe39f16eaae8fd4f78f1298e741c539
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2024-08-08 07:52:10 +0530

    Completed side project

--- Projects/JavaScript Projects/scroll/project7.js
+++
@@ -1,6 +0,0 @@
-window.onscroll = function () {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.querySelector(".scroll-indicator").style.width = scrolled + "%";
-}
