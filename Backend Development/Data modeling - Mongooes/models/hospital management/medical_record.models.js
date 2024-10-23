f61adaf6c13639348ed9357af40c6588d52190a2
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2024-10-23 16:16:57 +0530

    Removed

--- Backend Development/Data modeling - Mongooes/models/hospital management/medical_record.models.js
+++
@@ -1,8 +0,0 @@
-import mongooes from 'mongoose';
-
-const medicalRecordSchema = new mongooes.Schema({}, { timestamps: true });
-
-export const MedicalRecord = mongooes.model(
-  'MedicalRecord',
-  medicalRecordSchema
-);
